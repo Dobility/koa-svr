75f5ec8b7b98c7ca16e1d32bdd6eab00fe9d6740
Dobility <984687461@qq.com>
2019-09-19 10:15:44 +0800

    missing response.js

---
+++ middlewares/response.js
@@ -0,0 +1,30 @@
+import { logger } from './logger';
+
+// 这个middleware用于将ctx.result中的内容最终回传给客户端
+// 回传的格式遵循这样的格式：{ code: 0, msg: any, data: any }
+export const responseHandler = (ctx) => {
+  if (ctx.body) {
+    ctx.type = 'json';
+    ctx.body = {
+      code: 200,
+      success: true,
+      msg: ctx.msg || '',
+      data: ctx.body,
+    };
+  }
+};
+
+// 这个middleware处理在其它middleware中出现的异常
+// 并将异常消息回传给客户端：{ code: '错误代码', msg: '错误信息' }
+export const errorHandler = (ctx, next) => next().catch((err) => {
+  if (err.code == null) {
+    logger.error(err.stack);
+  }
+  ctx.body = {
+    code: err.code || -1,
+    success: false,
+    msg: err.message.trim(),
+  };
+  ctx.status = 200; // 保证返回状态是 200, 这样前端不会抛出异常
+  return Promise.resolve();
+});
